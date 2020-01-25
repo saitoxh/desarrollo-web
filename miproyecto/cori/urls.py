98438ae57d87a531f24dd3ccdf65bd26ad1f8da3
unknown <r.lefort@alumnos.duoc.cl>
2020-01-25 17:23:36 -0300

    creacion de resgistro

--- miproyecto/cori/urls.py
+++ miproyecto/cori/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from .views import home
+from .views import registro_usuario
 
 urlpatterns = [
     path('', home, name="home"),
+    path('registro/', registro_usuario, name='registro_usuario'),
 ]
--- miproyecto/cori/views.py
+++ miproyecto/cori/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CustomUserForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'cori/home.html')
+
+def registro_usuario(request):
+    data = {
+        'form' : CustomUserForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustomUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect(to = 'home')
+
+    return render(request, 'registration/registrar.html', data)
